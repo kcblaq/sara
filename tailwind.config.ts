30f4f7d73723299e25f6f860a770f141145923ba
Kelechi Ugwu <kcblack22@gmail.com>
2024-07-02 14:53:45 +0100

    Rectified tech seo tabs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,31 @@ const config: Config = {
         primary: '#1570EF',
         footerBlue: '#1849A9',
         darkPrimary:'#194185',
-        lightBg: '#F9FAFB'
+        lightBg: '#F9FAFB',
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      popover: {
+        DEFAULT: "hsl(var(--popover))",
+        foreground: "hsl(var(--popover-foreground))",
       },
     },
   },
