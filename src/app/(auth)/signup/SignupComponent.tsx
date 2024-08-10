31bd3eb6d3c5a4bc2f73326b2fc668d4f10388b4
josephDev123 <josephuzuegbu55@gmail.com>
2024-08-10 11:08:13 +0100

    fix login and signup design iteration

--- src/app/(auth)/signup/SignupComponent.tsx
+++ src/app/(auth)/signup/SignupComponent.tsx
@@ -1,6 +1,6 @@
-'use client'
-import Image from "next/image"
-import Link from "next/link"
+"use client";
+import Image from "next/image";
+import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
 import { CiMail } from "react-icons/ci";
 import { useState } from "react";
@@ -10,158 +10,209 @@ import * as Yup from "yup";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { BiLoader } from "react-icons/bi";
-
+import MobileLogoIcon from "../../../components/svgComponents/CompanyMobileLogo";
 
 export const SignupComponent = () => {
-    const [isPassword, setIsPassword] = useState('password')
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState({status: false, msg:''})
-    const router = useRouter()
+  const [isPassword, setIsPassword] = useState("password");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({ status: false, msg: "" });
+  const router = useRouter();
 
-    const Register = async()=> {
-        setLoading(true)
-            try {
-                await axios.post('https://api.webmaxi.net/api/auth/register', {
-                  fullName: formik.values.name,
-                  email: formik.values.email,
-                  password: formik.values.password
-                })
-                .then((res)=> {
-                    if(res.status == 201 ){
-                        sessionStorage.setItem("user",res.data)
-                        // console.log(res.data)
-                        router.push('/signup/email-verify/otp')
-                    } else {
-                        // console.log("RES", res)
-                        // setError({status:true, msg: res.data})
-                    }
-                } )
-                .then(()=> localStorage.setItem("userEmail", formik.values.email))
-            }
-
-          catch(err: any){
-            setError({status: true, msg:err.response.data.message})
+  const Register = async () => {
+    setLoading(true);
+    try {
+      await axios
+        .post("https://api.webmaxi.net/api/auth/register", {
+          fullName: formik.values.name,
+          email: formik.values.email,
+          password: formik.values.password,
+        })
+        .then((res) => {
+          if (res.status == 201) {
+            sessionStorage.setItem("user", res.data);
+            // console.log(res.data)
+            router.push("/signup/email-verify/otp");
+          } else {
+            // console.log("RES", res)
+            // setError({status:true, msg: res.data})
           }
-
-          const payload = {
-            fullName: formik.values.name,
-                      email: formik.values.email,
-                      password: formik.values.password
-        }
-    setLoading(false)
-     
-    }
-    
-    const formik = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            password: ''
-        },
-        validationSchema: Yup.object().shape({
-            name: Yup.string().required("Name is required").min(3, "Name should be atleast 3 letters"),
-            email: Yup.string().email('Please enter a valid email').required("Email is a required field"),
-            password: Yup.string().required().min(8, 'Password must be atleast 8 character')
-                .matches(
-                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
-                    'Password must contain a combination of capital and small letters, a digit, and a special character'
-                )
-        }),
-        onSubmit: Register
-    })
-
-    
-  
-    const currentYear = () => {
-        const currentDate = new Date();
-        return currentDate.getFullYear()
+        })
+        .then(() => localStorage.setItem("userEmail", formik.values.email));
+    } catch (err: any) {
+      setError({ status: true, msg: err.response.data.message });
     }
 
-    return (
-        <main className=" w-full h-full flex flex-col gap-10 lg:justify-between ">
+    const payload = {
+      fullName: formik.values.name,
+      email: formik.values.email,
+      password: formik.values.password,
+    };
+    setLoading(false);
+  };
 
-            <Link href={`/`}>
-            <Image src={`/logo.png`} alt="Webmaxi Logo" width={145} height={24} className="" />
-                {/* <Image src={`/logo 3.png`} alt="Webmaxi Logo" width={200} height={0} className="hidden lg:flex" />
-                <Image src={`/mobile-logo.png`} alt="Webmaxi Logo" width={20} height={0} className="flex lg:hidden" /> */}
-            </Link>
-            <form onSubmit={formik.handleSubmit}>
-                <section className="lg:px-20 sm:px-32  w-full justify-between flex flex-col gap-2">
-                    <div className="flex flex-col w-full my-2">
-                    <p className=" font-bold text-lg lg:text-2xl">Signup  </p>
-                    <small className="font-thin "> Start your free trial today</small>
-                    </div>
-                    <small className="text-red-500">{error.status && error.msg} </small>
-                    <label className="font-bold text-sm lg:text-base"> Name*</label>
-                    <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        value={formik.values.name}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        placeholder="Enter your name"
-                        className={`p-2 rounded-md border focus:outline-none ${formik.touched.name && formik.errors.name && 'border-red-400'} focus:shadow focus:shadow-primary  `} />
-                    {formik.touched.name && formik.errors.name && (
-                        <small className="text-red-500">{formik.errors.name}</small>
-                    )}
-                    <label
-                        className="font-bold text-sm lg:text-base">
-                        Email*</label>
-                    <input
-                        type="email"
-                        placeholder="Enter your email"
-                        name="email"
-                        id="email"
-                        value={formik.values.email}
-                        onBlur={formik.handleBlur}
-                        onChange={formik.handleChange}
-                        className={`p-2 rounded-md border focus:outline-none ${formik.touched.email && formik.errors.email && 'border-red-400'} focus:shadow focus:shadow-primary  `}  />
-                    {formik.touched.email && formik.errors.email && (
-                        <small className="text-red-500">{formik.errors.email}</small>
-                    )}
-                    <label
-                        className="font-bold text-sm lg:text-b">
-                        Password*
-                    </label>
-                    <span className="relative w-full">
-                        <input
-                            type={isPassword}
-                            placeholder="Create a password"
-                            name="password"
-                            id="password"
-                            value={formik.values.password}
-                            onBlur={formik.handleBlur}
-                            onChange={formik.handleChange}
-                            className={`p-2 rounded-md border focus:outline-none ${formik.touched.password && formik.errors.password && 'border-red-400'} focus:shadow focus:shadow-primary  w-full `}  />
-                        {formik.touched.password && formik.errors.password && (
-                            <small className="text-red-500">{formik.errors.password}</small>
-                        )}
-                        <span className="absolute right-2 top-3 cursor-pointer">
-                            {isPassword == "text" ?
-                                <span className="" onClick={() => setIsPassword('password')}><IoEyeOffOutline /> </span>
-                                : <span className="" onClick={() => setIsPassword('text')} > <IoEyeOutline /></span>} </span>
-                    </span>
-                    <small className=" font-thin -mt-2 " > Must be at least 8 characters.</small>
-                    <button
-                        className=" w-full p-2 flex items-center justify-center gap-2 font-bold bg-primary text-white rounded-md"
-                        type="submit"
-                    >
-                        Create
-                        account   {loading && <span> <BiLoader className="w-full  animate-spin text-white" /> </span> }
-                    </button>
-                    {/* <FilledButton title="Create Account" loading={loading} /> */}
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object().shape({
+      name: Yup.string()
+        .required("Name is required")
+        .min(3, "Name should be atleast 3 letters"),
+      email: Yup.string()
+        .email("Please enter a valid email")
+        .required("Email is a required field"),
+      password: Yup.string()
+        .required()
+        .min(8, "Password must be atleast 8 character")
+        .matches(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
+          "Password must contain a combination of capital and small letters, a digit, and a special character"
+        ),
+    }),
+    onSubmit: Register,
+  });
 
-                    {/* <span className=" cursor-pointer w-full p-2 font-bold rounded-md border flex items-center justify-center gap-2"><FcGoogle /> Signup with google </span> */}
-                    <Link href={`/login`}><small className=""> Already have an account? <span className=" text-primary font-bold"> Log in</span></small> </Link>
-                </section>
-            </form>
-            <footer className="lg:flex justify-between w-full items-center hidden ">
-                <small className=""> &#169;Webmaxi {currentYear()}.</small>
-                <small className="flex items-center gap-1"> <CiMail /> help@webmaxi.net</small>
-            </footer>
+  const currentYear = () => {
+    const currentDate = new Date();
+    return currentDate.getFullYear();
+  };
 
+  return (
+    <main className="xl:max-w-[800px]  lg:max-w-[600px] p-4 w-full h-full flex flex-col gap-10 lg:justify-between ">
+      <Link href={`/`}>
+        <Image
+          src={`/logo.png`}
+          alt="Webmaxi Logo"
+          width={145}
+          height={24}
+          className="hidden lg:flex"
+        />
+        <MobileLogoIcon className="size-11 -mb-6 lg:hidden" />
+      </Link>
 
-        </main>
-    )
-}
+      <form onSubmit={formik.handleSubmit}>
+        <section className="lg:px-20 sm:px-32  w-full justify-between flex flex-col gap-2 ">
+          <div className="flex flex-col w-full my-2">
+            <p className=" font-bold text-2xl lg:text-4xl">Signup </p>
+            <small className="mt-3 text-gray-600">
+              Start your free trial today.
+            </small>
+          </div>
+          <small className="text-red-500">{error.status && error.msg} </small>
+          <label className="font-bold text-sm lg:text-base"> Name*</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            placeholder="Enter your name"
+            className={`p-2 rounded-md border focus:outline-none ${
+              formik.touched.name && formik.errors.name && "border-red-400"
+            } focus:shadow focus:shadow-primary  `}
+          />
+          {formik.touched.name && formik.errors.name && (
+            <small className="text-red-500">{formik.errors.name}</small>
+          )}
+          <label className="font-bold text-sm lg:text-base">Email*</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            name="email"
+            id="email"
+            value={formik.values.email}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            className={`p-2 rounded-md border focus:outline-none ${
+              formik.touched.email && formik.errors.email && "border-red-400"
+            } focus:shadow focus:shadow-primary  `}
+          />
+          {formik.touched.email && formik.errors.email && (
+            <small className="text-red-500">{formik.errors.email}</small>
+          )}
+          <label className="font-bold text-sm lg:text-b">Password*</label>
+          <span className="relative w-full">
+            <input
+              type={isPassword}
+              placeholder="Create a password"
+              name="password"
+              id="password"
+              value={formik.values.password}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              className={`p-2 rounded-md border focus:outline-none ${
+                formik.touched.password &&
+                formik.errors.password &&
+                "border-red-400"
+              } focus:shadow focus:shadow-primary  w-full `}
+            />
+            {formik.touched.password && formik.errors.password && (
+              <small className="text-red-500">{formik.errors.password}</small>
+            )}
+            <span className="absolute right-2 top-3 cursor-pointer">
+              {isPassword == "text" ? (
+                <span className="" onClick={() => setIsPassword("password")}>
+                  <IoEyeOffOutline />{" "}
+                </span>
+              ) : (
+                <span className="" onClick={() => setIsPassword("text")}>
+                  {" "}
+                  <IoEyeOutline />
+                </span>
+              )}{" "}
+            </span>
+          </span>
+          <small className="text-gray-600 -mt-2 ">
+            {" "}
+            Must be at least 8 characters.
+          </small>
+          <button
+            className=" w-full p-2 mt-4 flex items-center justify-center gap-2 font-bold bg-primary text-white rounded-md"
+            type="submit"
+          >
+            Create account{" "}
+            {loading && (
+              <span>
+                {" "}
+                <BiLoader className="w-full  animate-spin text-white" />{" "}
+              </span>
+            )}
+          </button>
+          <button
+            className=" w-full p-2 flex items-center mt-2 justify-center gap-2 font-bold border text-black rounded-md"
+            type="submit"
+          >
+            <FcGoogle className="size-6" />
+            Sign up with Google
+            {loading && (
+              <span>
+                {" "}
+                <BiLoader className="w-full  animate-spin text-white" />{" "}
+              </span>
+            )}
+          </button>
+          {/* <FilledButton title="Create Account" loading={loading} /> */}
+
+          {/* <span className=" cursor-pointer w-full p-2 font-bold rounded-md border flex items-center justify-center gap-2"><FcGoogle /> Signup with google </span> */}
+          <Link href={`/login`} className="text-center mt-4">
+            <small className="">
+              {" "}
+              Already have an account?{" "}
+              <span className=" text-primary font-bold"> Log in</span>
+            </small>{" "}
+          </Link>
+        </section>
+      </form>
+      <footer className="lg:flex justify-between w-full items-center hidden mr-8">
+        <small className=""> &#169;Webmaxi {currentYear()}.</small>
+        <small className="flex items-center gap-1">
+          {" "}
+          <CiMail /> help@webmaxi.net
+        </small>
+      </footer>
+    </main>
+  );
+};
