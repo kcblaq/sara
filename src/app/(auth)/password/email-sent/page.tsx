827f264449ab6111beb8c35328fc4a8e9b3c8df9
josephDev123 <josephuzuegbu55@gmail.com>
2024-08-11 09:14:24 +0100

    reiterate auth pages

--- src/app/(auth)/password/email-sent/page.tsx
+++ src/app/(auth)/password/email-sent/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import BackToLogin from "../BackToLogin";
 import TitleAndDescription from "../../../component/TitleAndDescription";
 import FilledButton from "@/app/component/FilledButton";
@@ -9,34 +9,45 @@ import axios from "axios";
 // import { useDispatch } from "react-redux";
 import { useState } from "react";
 
-export default function EmailSent(){
-    const userEmail = useSelector((state:RootState)=> state.user.user.email);
-    const [text, setText] = useState('Click to resend')
-    // const route = useRouter()
-    // const dispatch = useDispatch()
-    
-    async function handleSubmit() {
-        setText('Sending...')
-        try {
-            
-            const res = await axios.post('https://api.webmaxi.net/api/auth/forgot-password', {
-                email: userEmail
-            });
-        } catch (error: any) {
-            console.error('Error submitting form:', error);
+export default function EmailSent() {
+  const userEmail = useSelector((state: RootState) => state.user.user.email);
+  const [text, setText] = useState("Click to resend");
+  // const route = useRouter()
+  // const dispatch = useDispatch()
+
+  async function handleSubmit() {
+    setText("Sending...");
+    try {
+      const res = await axios.post(
+        "https://api.webmaxi.net/api/auth/forgot-password",
+        {
+          email: userEmail,
         }
-        setText('Click to resend')
+      );
+    } catch (error: any) {
+      console.error("Error submitting form:", error);
     }
-    return (
-        <main className="flex flex-col gap-8 ">
-            <TitleAndDescription title="Check your email" description={`We set a password reset link to ${userEmail}`} />
-            {/* <button className=" bg-purple-700 rounded-md font-semibold text-white w-full p-2">
+    setText("Click to resend");
+  }
+  return (
+    <main className="flex flex-col gap-8">
+      <TitleAndDescription
+        title="Check your email"
+        description={`We sent a password reset link to ${userEmail}`}
+      />
+      {/* <button className=" bg-purple-700 rounded-md font-semibold text-white w-full p-2">
                 Open email app
             </button> */}
-            <FilledButton title="Open email app" />
-            <p> Didn't recieve the email? <span className=" text-purple-700 cursor-pointer" onClick={handleSubmit}> {text} </span></p>
+      <FilledButton title="Open email app" />
+      <p className="text-sm text-center">
+        <span className="text-gray-600">Didn't receive the email?</span>
+        <span className=" text-[#175CD3] cursor-pointer" onClick={handleSubmit}>
+          {" "}
+          {text}{" "}
+        </span>
+      </p>
 
-            <BackToLogin/>
-        </main>
-    )
+      <BackToLogin />
+    </main>
+  );
 }
--- src/app/(auth)/password/page.tsx
+++ src/app/(auth)/password/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Link from "next/link";
 import { useState } from "react";
 import { FaArrowLeft } from "react-icons/fa6";
@@ -10,90 +10,101 @@ import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
 import { setUser } from "@/redux/features/userSlice";
 
-
 export default function ForgetPassword() {
-    const [loading, setLoading] = useState(false);
-    const [message, setMessage] = useState({
-        status: false,
-        msg:''
-    })
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState({
+    status: false,
+    msg: "",
+  });
 
-    const dispatch = useDispatch()
-    const formik = useFormik({
-        initialValues: {
-            email: ''
-        },
-        validationSchema: Yup.object().shape({
-            email: Yup.string().email('Enter a valid email address').required('Please enter an email address')
-        }),
-        onSubmit: handleSubmit
-    })
+  const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string()
+        .email("Enter a valid email address")
+        .required("Please enter an email address"),
+    }),
+    onSubmit: handleSubmit,
+  });
 
-const route = useRouter()
-    async function handleSubmit(values: { email: string }, formikHelpers: FormikHelpers<{ email: string }>) {
-        setLoading(true);
-        try {
-            const res = await axios.post('https://api.webmaxi.net/api/auth/forgot-password', {
-                email: values.email
-            });
-            setLoading(false);
-            if(res.status == 201){
-                dispatch(setUser({email:formik.values.email}))
-                setMessage({status: true, msg: 'Reset link sent'})
-                setTimeout(()=> {
-                    route.push('/password/email-sent')
-                    setMessage({status: false, msg:''})
-                },6000)
-            }
-
-        } catch (error: any) {
-            console.error('Error submitting form:', error);
-            setLoading(false);
-            
-                setMessage({status: true, msg: error.message})
-                setTimeout(()=> {
-                    setMessage({status: false, msg:''})
-                },6000)
-            
+  const route = useRouter();
+  async function handleSubmit(
+    values: { email: string },
+    formikHelpers: FormikHelpers<{ email: string }>
+  ) {
+    setLoading(true);
+    try {
+      const res = await axios.post(
+        "https://api.webmaxi.net/api/auth/forgot-password",
+        {
+          email: values.email,
         }
+      );
+      setLoading(false);
+      if (res.status == 201) {
+        dispatch(setUser({ email: formik.values.email }));
+        setMessage({ status: true, msg: "Reset link sent" });
+        setTimeout(() => {
+          route.push("/password/email-sent");
+          setMessage({ status: false, msg: "" });
+        }, 6000);
+      }
+    } catch (error: any) {
+      console.error("Error submitting form:", error);
+      setLoading(false);
+
+      setMessage({ status: true, msg: error.message });
+      setTimeout(() => {
+        setMessage({ status: false, msg: "" });
+      }, 6000);
     }
-    
-    return (
-        <main className=" w-full justify-center items-center flex gap-4 flex-col">
-            <h1 className=" font-bold text-3xl">Forgot password? </h1>
-            { message.status === false && <p className="w-full">No worries, we will send you instructions. </p>}
-            { message.status !== false && <p className="w-full"> {message.msg} </p>}
-            <form onSubmit={formik.handleSubmit} className="w-full flex flex-col gap-4">
-                <div className="flex flex-col w-full">
+  }
 
-                    <label className="" id="email"> Email</label>
-                    <input
-                        className="p-2 border rounded-md focus:outline-primary w-full"
-                        value={formik.values.email}
-                        onChange={formik.handleChange}
-                        type="text"
-                        placeholder="Enter your email"
-                        onBlur={formik.handleBlur}
-                        id="email"
-                        name="email"
-
-
-                    />
-                    {formik.touched.email && formik.errors.email && (
-                        <small className="text-red-500">{formik.errors.email}</small>
-                    )}
-
-                </div>
-                <FilledButton title="Reset password" loading={loading} type="submit" />
-            </form>
-            {/* <button className=" bg-primary w-full rounded-md text-white p-2">
+  return (
+    <main className=" w-full justify-center items-center flex gap-4 flex-col">
+      <h1 className=" font-bold sm:text-3xl text-2xl">Forgot password? </h1>
+      {message.status === false && (
+        <p className="w-full text-base text-center text-gray-600">
+          No worries, we will send you instructions.{" "}
+        </p>
+      )}
+      {message.status !== false && <p className="w-full"> {message.msg} </p>}
+      <form
+        onSubmit={formik.handleSubmit}
+        className="w-full flex flex-col gap-4"
+      >
+        <div className="flex flex-col w-full">
+          <label className="" id="email">
+            {" "}
+            Email
+          </label>
+          <input
+            className="p-2 border rounded-md focus:outline-primary w-full"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            type="text"
+            placeholder="Enter your email"
+            onBlur={formik.handleBlur}
+            id="email"
+            name="email"
+          />
+          {formik.touched.email && formik.errors.email && (
+            <small className="text-red-500">{formik.errors.email}</small>
+          )}
+        </div>
+        <FilledButton title="Reset password" loading={loading} type="submit" />
+      </form>
+      {/* <button className=" bg-primary w-full rounded-md text-white p-2">
                 Reset password
             </button> */}
 
-            <Link href={`/login`} className="flex items-center gap-2">
-                <FaArrowLeft />
-                Back to login
-            </Link>
-        </main>
-    )
+      <Link href={`/login`} className="flex items-center gap-2 text-sm mt-4">
+        <FaArrowLeft />
+        Back to login
+      </Link>
+    </main>
+  );
 }
