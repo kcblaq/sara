88ffce90a941fd313d3444ac1298003be102b245
josephDev123 <josephuzuegbu55@gmail.com>
2024-08-12 05:59:26 +0100

    iterate email verify

--- src/app/(auth)/signup/email-verify/otp/OtpInput.tsx
+++ src/app/(auth)/signup/email-verify/otp/OtpInput.tsx
@@ -1,15 +1,16 @@
-import FilledButton from '@/app/component/FilledButton';
-import { AxiosInstance } from '@/lib/axios';
-import { setToken, setUser } from '@/redux/features/userSlice';
-import { useRouter } from 'next/navigation';
-import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
-import { useDispatch } from 'react-redux';
-
+import FilledButton from "@/app/component/FilledButton";
+import { AxiosInstance } from "@/lib/axios";
+import { setToken, setUser } from "@/redux/features/userSlice";
+import { useRouter } from "next/navigation";
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+import { useDispatch } from "react-redux";
 
 const OtpInput: React.FC = () => {
-  const inputRefs = Array.from({ length: 6 }, () => useRef<HTMLInputElement>(null));
-  const [otpValues, setOtpValues] = useState<string[]>(Array(6).fill(''));
-  const [loading, setLoading] = useState(false)
+  const inputRefs = Array.from({ length: 6 }, () =>
+    useRef<HTMLInputElement>(null)
+  );
+  const [otpValues, setOtpValues] = useState<string[]>(Array(6).fill(""));
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     // Set initial focus on the first input field when the component mounts
@@ -28,88 +29,99 @@ const OtpInput: React.FC = () => {
     setOtpValues(newOtpValues);
   };
 
-  const handleKeyDown = (index: number, e: React.KeyboardEvent<HTMLInputElement>) => {
+  const handleKeyDown = (
+    index: number,
+    e: React.KeyboardEvent<HTMLInputElement>
+  ) => {
     const value = e.currentTarget.value;
 
-    if (e.key === 'ArrowRight' && index < 5) {
+    if (e.key === "ArrowRight" && index < 5) {
       inputRefs[index + 1].current?.focus();
-    } else if ((e.key === 'ArrowLeft' && index > 0) || (e.key === 'Backspace' && !value)) {
+    } else if (
+      (e.key === "ArrowLeft" && index > 0) ||
+      (e.key === "Backspace" && !value)
+    ) {
       inputRefs[index - 1].current?.focus();
     }
 
     const newOtpValues = [...otpValues];
-    if (e.key !== 'Backspace') {
+    if (e.key !== "Backspace") {
       newOtpValues[index] = value;
     }
     setOtpValues(newOtpValues);
   };
-const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const handlePaste = (e: React.ClipboardEvent<HTMLInputElement>) => {
     e.preventDefault();
-    const pastedData = e.clipboardData.getData('text/plain').slice(0, 6);
+    const pastedData = e.clipboardData.getData("text/plain").slice(0, 6);
 
     // Distribute each character from pastedData to the corresponding input field
-    const newOtpValues = Array(6).fill('').map((_, index) => pastedData[index] || '');
+    const newOtpValues = Array(6)
+      .fill("")
+      .map((_, index) => pastedData[index] || "");
     setOtpValues(newOtpValues);
   };
 
-  const token = localStorage.getItem('token');
-  const router = useRouter()
-  
-  
-  const route = useRouter()
-  const userEmail = localStorage.getItem("userEmail")
+  const token = localStorage.getItem("token");
+  const router = useRouter();
+
+  const route = useRouter();
+  const userEmail = localStorage.getItem("userEmail");
   const payload = {
     email: userEmail,
-    otp: parseInt(otpValues.join(""),10)
+    otp: parseInt(otpValues.join(""), 10),
+  };
+
+  async function VerifyEmail() {
+    console.log(JSON.stringify(token));
+    setLoading(true);
+    try {
+      await AxiosInstance.post("/auth/verify-otp", payload)
+        .then((res) => {
+          if (res.status == 200) {
+            console.log("RES::", res.data.token);
+            dispatch(setUser(res.data.user));
+            dispatch(setToken(res.data.token));
+            router.push("/dashboard");
+          }
+        })
+        .then(() => console.log("Submitted!"));
+    } catch (err) {
+      console.log(err);
+    }
+    setLoading(false);
   }
 
- 
-  
- async function VerifyEmail(){
-   console.log(JSON.stringify(token))
-   setLoading(true)
-   try{
-   
-    await AxiosInstance.post('/auth/verify-otp', payload)
-     .then((res) => {
-      if(res.status == 200){
-        console.log("RES::",res.data.token)
-        dispatch(setUser(res.data.user))
-        dispatch(setToken(res.data.token))
-            router.push('/dashboard')
-      }
-     })
-     .then(()=> console.log('Submitted!'))
-   }
-   catch(err){
-    console.log(err)
-   }
-   setLoading(false)
-  }
- 
   return (
-    <div className="flex space-x-2 flex-col gap-4 w-full">
-      <div className='flex space-x-2 justify-center '>
-      {Array.from({ length: 4 }, (_, index) => (
-        <input
-          key={index}
-          ref={inputRefs[index]}
-          type="text"
-          maxLength={1}
-          onChange={(e) => handleInput(index, e)}
-          onKeyDown={(e) => handleKeyDown(index, e)}
-          onPaste={handlePaste}
-          value={otpValues[index]}
-          className="w-10 h-10 text-center border rounded-md focus:outline-none focus:shadow focus:shadow-primary"
-        />
-      ))}
+    <div className="flex space-x-2 flex-col gap-4 min-[425px]:w-full w-[90%]">
+      <div className="flex space-x-3 justify-center my-4">
+        {Array.from({ length: 4 }, (_, index) => (
+          <input
+            key={index}
+            ref={inputRefs[index]}
+            placeholder="0"
+            type="text"
+            maxLength={1}
+            onChange={(e) => handleInput(index, e)}
+            onKeyDown={(e) => handleKeyDown(index, e)}
+            onPaste={handlePaste}
+            value={otpValues[index]}
+            className="  min-[425px]:size-20 size-16 text-5xl text-primary text-center border rounded-md focus:outline-none focus:shadow focus:shadow-primary"
+          />
+        ))}
       </div>
-      <FilledButton loading={loading} title='Verify email' handleClick={VerifyEmail} />
+      <FilledButton
+        loading={loading}
+        title="Verify email"
+        handleClick={VerifyEmail}
+      />
+
+      <p className="text-center text-sm">
+        Didn’t receive the email?{" "}
+        <button className="text-primary">Click to resend </button>
+      </p>
     </div>
   );
 };
 
 export default OtpInput;
-
-
---
+++ src/app/(auth)/signup/email-verify/verify-success/page.tsx
@@ -0,0 +1,28 @@
+import FilledButton from "@/app/component/FilledButton";
+import TitleAndDescription from "@/app/component/TitleAndDescription";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function page() {
+  // const route = useRouter();
+  return (
+    <div className="flex flex-col gap-4 min-[425px]:min-w-[400px] w-full">
+      <TitleAndDescription
+        title="Email verified"
+        description={
+          <>
+            Your Email has been verify successfully. <br /> Click below to log
+            into your dashboard.
+          </>
+        }
+      />
+      <Link href={"/dashboard"}>
+        <FilledButton
+          title="Continue"
+          // handleClick={() => route.push("/dashboard")}
+        />
+      </Link>
+    </div>
+  );
+}
