889c19588f40bc048d24f65ca08dfae16a4be2f7
Kelechi Ugwu <kcblack22@gmail.com>
2024-09-27 20:18:11 +0100

    Some rank changes

--- src/app/dashboard/technical-seo/components/OrganicTrafficCard.tsx
+++ src/app/dashboard/technical-seo/components/OrganicTrafficCard.tsx
@@ -1,130 +1,50 @@
-import { useSelector } from "react-redux";
 import Card from "../../Card";
-import { RootState } from "@/app/store";
-import { calculatePercentage, millisecondsToSeconds } from "@/lib/DateFormater";
-import { Scores } from "@/types/DashboardOverview";
+import { calculatePercentageDifference } from "@/lib/DateFormater";
 import { ShortenNumber } from "@/app/utils/ShortenedNumber";
 import { LineChart } from "./LineChart";
+import { useRankTrackingOverview } from "@/app/services/crawlers/rank_tracking";
 
 export default function OrganicTrafficCard() {
-  const { loading, error, metrics } = useSelector(
-    (state: RootState) => state.performance
-  );
-  const scores = metrics?.history?.scores;
 
-  let actual: Scores | undefined;
-  let previosUpdate: Scores | undefined;
+  const { isError, isPending, data: OverviewData } = useRankTrackingOverview();
+  const organicTraffic = OverviewData?.project?.crawlings[0]?.crawlingData[0]?.data?.google?.organic_traffic ?? 0;
+  const previousVal = OverviewData?.project?.crawlings[1]?.crawlingData[0]?.data?.google?.organic_traffic ?? 0;
 
-  const actualOrganicTraffic =
-    metrics?.history?.scores[0]?.organic_traffic ?? 0;
-  if (scores && scores.length >= 2) {
-    actual = scores[0];
-    previosUpdate = scores[1];
+  const percentageDiff = calculatePercentageDifference(previousVal, organicTraffic);
+
+  function getFirstFiveOrganicTraffic() {
+    const organicTrafficArray = [];
+    const crawlings = OverviewData?.project?.crawlings || [];
+
+    for (let i = 0; i < crawlings.length && organicTrafficArray.length < 5; i++) {
+      const crawlingData = crawlings[i]?.crawlingData || [];
+      for (let j = 0; j < crawlingData.length && organicTrafficArray.length < 5; j++) {
+        const organicTraffic = crawlingData[j]?.data?.google?.organic_traffic;
+        if (organicTraffic !== undefined) {
+          organicTrafficArray.push(organicTraffic);
+        }
+      }
+    }
+
+    return organicTrafficArray;
   }
 
-  const style =
-    scores?.length === 1
-      ? "text-gray-500"
-      : scores &&
-        scores.length >= 2 &&
-        scores[1]?.organic_traffic !== undefined &&
-        scores[0]?.organic_traffic !== undefined
-      ? scores[1].organic_traffic < scores[0].organic_traffic
-        ? "text-green-500"
-        : scores[1].organic_traffic === scores[0].organic_traffic
-        ? "text-gray-500"
-        : "text-red-500"
-      : "text-red-500";
+  // console.log("FIRSTFIV:",getFirstFiveOrganicTraffic())
 
-  const TrafficIncreasePercentage = (
-    previousTraffic: number | undefined,
-    currentTraffic: number | undefined
-  ) => {
-    if (previosUpdate && actual) {
-      if (previousTraffic !== undefined && currentTraffic !== undefined) {
-        const trafficIncrease = currentTraffic - previousTraffic;
-        const trafficIncreasePercentage = calculatePercentage(
-          trafficIncrease,
-          previousTraffic
-        );
-        return trafficIncreasePercentage;
-      } else {
-        return 0;
-      }
-    } else {
-      return 0;
-    }
-  };
-
-  // const backgroundColor = scores?.length === 1
-  // ? 'gray'
-  // : scores && scores.length >= 2 && scores[0]?.organic_traffic === scores[1]?.organic_traffic
-  // ? 'gray'
-  // : scores && scores.length >= 2 && scores[0]?.organic_traffic && scores[1]?.organic_traffic
-  // ? scores[0].organic_traffic > scores[1].organic_traffic
-  //   ? '#ECFDF3'
-  //   : 'red'
-  // : 'gray';
-
-  // function extractFields(): any[] {
-  //     const result: any[] = [];
-  //     const newscoreLength = scores ? scores.length - 4 : 0; // Use scores?.length - 4 or default to 0 if scores is null or undefined
-  //     const startIndex = Math.max(0, newscoreLength); // Start index for the last four items
-
-  //     for (let i = startIndex; i < (scores?.length ?? 0); i++) { // Use scores?.length ?? 0 to handle null or undefined scores
-  //         const item = scores && scores[i];
-  //         if (item && item.hasOwnProperty('organic_traffic')) {
-  //             result.push(item.organic_traffic);
-  //         }
-  //     }
-
-  //     return result;
-  // }
-
-  // real implementation
-  // const pageData = (scores ?? [])
-  //   .map((item) => item.organic_traffic)
-  //   .filter((value) => value !== undefined) as number[];
-
-  // mock page data
-  const pageData = [4, 20, 30, 35, 45];
-
-  const per = millisecondsToSeconds(
-    TrafficIncreasePercentage(
-      previosUpdate?.average_time_on_site,
-      actual?.average_time_on_site
-    )
-  )?.toPrecision(2);
-  const checkPercentage = TrafficIncreasePercentage(
-    previosUpdate?.organic_traffic,
-    actual?.organic_traffic
-  );
-  // console.log("PAGED", pageData)
   return (
     <Card
+      isLoading={isPending}
+      isError={isError}
       title={"Organic Traffic"}
-      amount={ShortenNumber(actualOrganicTraffic)}
+      amount={ShortenNumber(organicTraffic) ?? 0}
       style={
-        pageData.length === 1
-          ? ""
-          : pageData[0] > pageData[pageData.length - 1]
-          ? "text-green-500"
-          : pageData[0] === pageData[pageData.length - 1]
-          ? "text-gray-500"
-          : "text-red-500"
+        percentageDiff === 0 ? "text-gray-500"
+          : percentageDiff > 0 ? "text-green-500" : "text-red-500"
       }
-      percent={checkPercentage}
-      chart={<LineChart pageData={pageData} />}
+      percent={percentageDiff}
+      chart={<LineChart pageData={getFirstFiveOrganicTraffic()} />}
       arrowPosition={
-        pageData.length === 1
-          ? ""
-          : pageData[0] > pageData[pageData.length - 1]
-          ? ""
-          : pageData[0] < pageData[1]
-          ? "rotate-180"
-          : pageData[0] === pageData[pageData.length - 1]
-          ? ""
-          : ""
+        percentageDiff < 0 ? "rotate-180" : ""
       }
     />
   );
