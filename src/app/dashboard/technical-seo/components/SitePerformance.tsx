bc854f9e9649edb2b4d4dd17b0915a8f37134f71
josephDev123 <josephuzuegbu55@gmail.com>
2024-11-07 10:36:38 +0100

    page load speed bar

--- src/app/dashboard/technical-seo/components/SitePerformance.tsx
+++ src/app/dashboard/technical-seo/components/SitePerformance.tsx
@@ -1,7 +1,10 @@
 import { RxQuestionMarkCircled } from "react-icons/rx";
 import Card from "../../Card";
 import BarChartSingle from "./(technicalseo)/BarChartSingle";
-import { QuadProgressBar } from "./(technicalseo)/DualProgressBar";
+import {
+  ProgressBarChart,
+  QuadProgressBar,
+} from "./(technicalseo)/DualProgressBar";
 
 import { SitePerformanceType } from "@/types/technicalseo/SitePerformance";
 import { ConvertToMilliuseconds } from "@/app/utils/ConvertToMilliseconds";
@@ -56,16 +59,17 @@ export default function SitePerformance() {
   const sitePerformanceIssue =
     sitePerformanceData[0]?.data.performance_issues || [];
 
-  const [metric1, metric2, metric3, metric4] = pageloadSpeedArray.slice(0, 4);
-
+  // const [metric1, metric2, metric3, metric4] = pageloadSpeedArray.slice(0, 4);
+  const pageloadSpeedArr = pageloadSpeedArray.slice(0, 4);
   // Calculate the total sum
-  const total = metric1 + metric2 + metric3 + metric4;
+  // const total = metric1 + metric2 + metric3 + metric4;
+  console.log(pageloadSpeedArray);
 
   // Convert the numeric values to percentage strings
-  const metric1Percentage = `${((metric1 / total) * 100).toFixed(2)}%`;
-  const metric2Percentage = `${((metric2 / total) * 100).toFixed(2)}%`;
-  const metric3Percentage = `${((metric3 / total) * 100).toFixed(2)}%`;
-  const metric4Percentage = `${((metric4 / total) * 100).toFixed(2)}%`;
+  // const metric1Percentage = `${((metric1 / total) * 100).toFixed(2)}%`;
+  // const metric2Percentage = `${((metric2 / total) * 100).toFixed(2)}%`;
+  // const metric3Percentage = `${((metric3 / total) * 100).toFixed(2)}%`;
+  // const metric4Percentage = `${((metric4 / total) * 100).toFixed(2)}%`;
 
   const CardClone = (
     <div className="grid gap-4 w-full md:max-w-[390px] h-[226px] rounded-md p-6 pb-2 border">
@@ -81,64 +85,16 @@ export default function SitePerformance() {
         <p className=" text-sm font-normal">{pageloadSpeedTotal} </p>
       </div>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 ">
         {/* <DualProgressBar leftPercentage={'30'} /> */}
-        <QuadProgressBar
+        {/* <QuadProgressBar
           metric1Percentage={metric1Percentage}
           metric2Percentage={metric2Percentage}
           metric3Percentage={metric3Percentage}
           metric4Percentage={metric4Percentage}
-        />
-        <div className="flex flex-col items-stretch justify-end w-full text-sm text-[#475467]">
-          {/* {data &&
-            Object.entries(data?.pageLoadSpeed).map(([key, value], index) => {
-              return (
-                <span
-                  key={index}
-                  className={`flex items-center gap-4 w-full justify-end`}
-                >
-                  <span
-                    className={` 
-                ${index === 0 && "bg-[#12B76A]"} 
-                ${index === 1 && "bg-[#2E90FA]"} 
-                ${index === 2 && "bg-[#D1E9FF]"} 
-                ${index === 3 && "bg-[#FEDF89]"} 
-                h-2 w-2 rounded-full`}
-                  >
-                    {" "}
-                  </span>{" "}
-                  {key} <b>({value as number} )</b>{" "}
-                </span>
-              );
-            })} */}
-          {/* {pageloadSpeedArray > 0 &&
-            pageloadSpeedArray &&
-            pageloadSpeedArray.map((value, index) => (
-              <span
-                key={index}
-                className="flex items-center gap-4 w-full justify-end"
-              >
-                <span
-                  className={`h-2 w-2 rounded-full ${
-                    index === 0
-                      ? "bg-[#12B76A]"
-                      : index === 1
-                      ? "bg-[#2E90FA]"
-                      : index === 2
-                      ? "bg-[#D1E9FF]"
-                      : index === 3
-                      ? "bg-[#FEDF89]"
-                      : ""
-                  }`}
-                ></span>
-                Page {index + 1} <b>({value})</b>
-              </span>
-            ))} */}
-
-          {/* <span className="flex items-center gap-4 w-full justify-end"> <span className=" bg-[#2E90FA] h-2 w-2 rounded-full" > </span> 0 - 0.5sec <b>(1,200)</b> </span>
-          <span className="flex items-center gap-4 w-full justify-end"> <span className=" bg-[#D1E9FF] h-2 w-2 rounded-full" > </span> 0 - 0.5sec <b>(2,300)</b> </span>
-          <span className="flex items-center gap-4 w-full justify-end"> <span className=" bg-[#FEDF89] h-2 w-2 rounded-full" > </span> 0 - 0.5sec <b>(1,100)</b> </span> */}
-        </div>
+        /> */}
+        <ProgressBarChart dataArray={pageloadSpeedArr} />
+        {/* <div className="flex flex-col items-stretch justify-end w-full text-sm text-[#475467]"></div> */}
       </div>
     </div>
   );
@@ -150,9 +106,7 @@ export default function SitePerformance() {
   ) : (
     <main className="grid gap-6 mt-10 h-full">
       <section className=" grid w-full grid-cols-1 md:grid-cols-3 md:gap-6 md:space-y-0 space-y-4 2xl:gap-8 py-4 mb-5">
-        {/* <Suspense fallback={<div className=""> Loading...</div>}> */}
         <div className="grid col-span-1 ">
-          {/* <Card title={"Average page load speed"} amount={performanceData ? performanceData?.data[1]?.performance : 0} style={""} percent={1} chart={undefined} /> */}
           <div className="flex md:flex-col min-[600px]:flex-row flex-col gap-7 ">
             <Card
               title={"Average page load speed"}
@@ -164,7 +118,6 @@ export default function SitePerformance() {
             {CardClone}
           </div>
         </div>
-        {/* </Suspense> */}
         <div className="col-span-2 border grid rounded-md p-6">
           <div className="flex flex-col w-full">
             <h1
@@ -188,17 +141,15 @@ export default function SitePerformance() {
         </div>
       </section>
       <section className="flex flex-col w-3/4 border rounded-md p-6 mb-20">
-        <div className="flex justify-between w-full border-b pb-2">
+        <div className="flex  justify-between w-full border-b pb-2">
           <h3 className="text-[#101828] text-xl font-semibold">
-            {" "}
             Site performance issues
           </h3>
           <button
             onClick={() => setIsPerformanceIssue(!isPerformanceIssue)}
             className="text-blue-400 cursor-pointer"
           >
-            {" "}
-            Show{" "}
+            Show
           </button>
         </div>
         <div className="">
